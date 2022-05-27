a905aafd00bd72dd33340b6c0d49df0a265afb48
cdlavacudeg <cristianlav10@gmail.com>
2022-05-27 16:58:25 -0500

    Express middleware version 2

--- user-auth/conceptual/middleware/app.js
+++ user-auth/conceptual/middleware/app.js
@@ -2,14 +2,41 @@ const express=require('express')
 
 const app = express()
 
+app.use(middleware1)// initilize middleware1 adign to the chain in all of the requests
+app.use(middleware3)
+app.use(middleware2)// order matters
+
 function middleware1(req,res,next) {
     console.log('I am a middleware')
     next()
 }
-function standardExpressCallback(req,res,nextMiddleware) {
-    console.log('I am the standard Express function')
-    res.send('<h1>Hello World</h1>')
+
+function middleware2(req,res,next) {
+    console.log('I am a second middleware')
+    next()
 }
 
-app.get('/',middleware1,standardExpressCallback)
+function middleware3(req,res,next) {
+    console.log('I am a third middleware')
+    next()
+}
+
+function middleware4(req,res,next) {
+    console.log('I am a fourth middleware')
+    next()
+}
+
+
+
+app.get('/',middleware4,(req,res,next)=>{
+    console.log('I am the standard Express function')
+    res.send('<h1>Hello World</h1>')
+})
+
+app.get('/no4',(req,res,next)=>{
+    console.log('I am the standard Express function')
+    res.send('<h1>Hello World</h1>')
+})
+
+
 app.listen(3000)
