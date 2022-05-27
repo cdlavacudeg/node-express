7e3ffdb227dceceef080a9572349a9083971b8f6
cdlavacudeg <cristianlav10@gmail.com>
2022-05-27 17:06:10 -0500

    Express middleware Error handling

--- user-auth/conceptual/middleware/app.js
+++ user-auth/conceptual/middleware/app.js
@@ -8,7 +8,9 @@ app.use(middleware2)// order matters
 
 function middleware1(req,res,next) {
     console.log('I am a middleware')
-    next()
+
+    const errObj=new Error('I am an error')
+    next(errObj)
 }
 
 function middleware2(req,res,next) {
@@ -26,7 +28,13 @@ function middleware4(req,res,next) {
     next()
 }
 
+// Error handling
 
+function errorHandler(err,req,res,next){
+    if(err){
+        res.send('<h1> There was an error, please try again</h1>')
+    }
+}
 
 app.get('/',middleware4,(req,res,next)=>{
     console.log('I am the standard Express function')
@@ -39,4 +47,5 @@ app.get('/no4',(req,res,next)=>{
 })
 
 
+app.use(errorHandler)
 app.listen(3000)
