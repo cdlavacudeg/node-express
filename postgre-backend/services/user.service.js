cb4f0557a2043b68c7f35d37118993807ce8f121
cdlavacudeg <cristianlav10@gmail.com>
2022-07-09 13:43:19 -0500

    [N-Postgres] Sequilize CRUD operations

--- postgre-backend/services/user.service.js
+++ postgre-backend/services/user.service.js
@@ -1,30 +1,37 @@
-const pool = require('../libs/postgres');
+const { models } = require('../libs/sequelize');
+const boom = require('@hapi/boom');
 
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
-    const query = 'SELECT * FROM task';
-    const table = await pool.query(query);
-    return table.rows;
+    const res = await models.User.findAll();
+    return res;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const res = await user.update(changes);
+    return res;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
