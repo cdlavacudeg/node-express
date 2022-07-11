0435754831280c79d9a8e0eaff37067e65fc3e48
cdlavacudeg <cristianlav10@gmail.com>
2022-07-11 13:52:07 -0500

    [N-Postgres] One-To-Many Association Sequelize P2

--- postgre-backend/services/product.service.js
+++ postgre-backend/services/product.service.js
@@ -1,12 +1,11 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
-const sequelize = require('../libs/sequelize');
+const { models } = require('../libs/sequelize');
 
 class ProductsService {
   constructor() {
     this.products = [];
     this.generate();
-    this.sequelize = sequelize;
   }
 
   generate() {
@@ -23,18 +22,16 @@ class ProductsService {
   }
 
   async create(data) {
-    const newProduct = {
-      id: faker.datatype.uuid(),
-      ...data,
-    };
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data);
+
     return newProduct;
   }
 
   async find() {
-    const query = 'SELECT * FROM task';
-    const [data] = await this.sequelize.query(query);
-    return data;
+    const products = await models.Product.findAll({
+      include: ['category']
+    });
+    return products;
   }
 
   async findOne(id) {
