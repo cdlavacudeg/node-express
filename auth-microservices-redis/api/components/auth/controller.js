e2f112ac4149b0bb3d96da5e2f369f2a1c1ac72c
cdlavacudeg <cristianlav10@gmail.com>
2022-07-21 09:51:46 -0500

    [Auth-Redis] Authentication: Register

---
+++ auth-microservices-redis/api/components/auth/controller.js
@@ -0,0 +1,27 @@
+TABLE = "auth";
+module.exports = function (injecteStore) {
+  let store = injecteStore;
+  if (!store) {
+    store = require("../../../store/dummy");
+  }
+
+  function upsert(data) {
+    const authData = {
+      id: data.id,
+    };
+
+    if (data.username) {
+      authData.username = data.username;
+    }
+
+    if (data.password) {
+      authData.password = data.password;
+    }
+
+    return store.upsert(TABLE, authData);
+  }
+
+  return {
+    upsert,
+  };
+};
--- auth-microservices-redis/api/components/user/controller.js
+++ auth-microservices-redis/api/components/user/controller.js
@@ -1,4 +1,5 @@
 const uniqid = require("uniqid");
+const auth = require("../auth");
 const TABLE = "user";
 
 module.exports = function (injecteStore) {
@@ -12,8 +13,10 @@ module.exports = function (injecteStore) {
   function get(id) {
     return store.get(TABLE, id);
   }
-  function upsert(body) {
+
+  async function upsert(body) {
     const user = {
+      username: body.username,
       name: body.name,
     };
 
@@ -21,6 +24,14 @@ module.exports = function (injecteStore) {
       user.id = body.id;
     } else {
       user.id = uniqid();
+    }
+
+    if (body.password || body.username) {
+      await auth.upsert({
+        id: user.id,
+        username: user.username,
+        password: body.password,
+      });
     }
     return store.upsert(TABLE, user);
   }
