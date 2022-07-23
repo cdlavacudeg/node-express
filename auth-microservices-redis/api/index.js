202f5fdcf057ce7b52fe348e0611ec09b783e8df
cdlavacudeg <cristianlav10@gmail.com>
2022-07-23 12:22:16 -0500

    [Auth-Redis]Post microservice

--- auth-microservices-redis/api/index.js
+++ auth-microservices-redis/api/index.js
@@ -7,7 +7,6 @@ const config = require("../config.js");
 
 const user = require("./components/user/network");
 const auth = require("./components/auth/network");
-const post = require("./components/post/network");
 const errors = require("../network/errors.js");
 //app.use(urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +15,6 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use("/api/user", user);
 app.use("/api/auth", auth);
-app.use("/api/post", post);
 
 app.use(errors);
 app.listen(config.api.port, () => {
